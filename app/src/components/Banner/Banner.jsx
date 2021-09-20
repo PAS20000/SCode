049060d7a118ba7499db1433f659b173679359c1
PAS818 <83708869+PAS19@users.noreply.github.com>
2021-09-20 20:09:46 -0300

    banner

--- app/src/components/Banner/Banner.jsx
+++ app/src/components/Banner/Banner.jsx
@@ -9,8 +9,11 @@ const theme = useContext(ThemeContext)
     return (
         <div>
             <NextImage src={theme.Title === 'light' ? '/img/ScodeCardL.png':'/img/ScodeCardD.png'} 
-            width={3500} height={1400} alt='CardD' layout='responsive' 
-            className={theme.Title === 'light' ? 'back':'back'}/>           
+            width={3500} height={1400} alt='Card' layout='responsive' 
+            className='back' />
+            <NextImage src={theme.Title === 'light' ? '/img/storeL.png':'/img/storeD.png'}
+            width={3500} height={1400} alt='Store' layout='responsive' 
+            className='back' />           
         </div>
     )
 }
