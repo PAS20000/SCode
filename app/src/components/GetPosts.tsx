afbb8be88f936427703934b4fe9521032a302baf
PAS818 <83708869+PS200000@users.noreply.github.com>
2022-04-14 15:34:50 -0300

    useStaticPagination v1.0

--- app/src/components/GetPosts.tsx
+++ app/src/components/GetPosts.tsx
@@ -1,15 +1,37 @@
 import { SimpleGrid } from "@chakra-ui/react";
 import CustomHeading from "./CustomHeading";
 import PostCard from "./PostCard";
-import {posts} from '../../pages/api/posts'
+import { posts } from '../../pages/api/posts'
+import useStaticPagination from "../hooks/useStaticPagination/useStaticPagination";
 
 export default function GetPosts() {
+    /*const [posts, setPosts] = useState([])
 
+    useEffect(() => {
+        (async function() {
+            const resp = await fetch('api_url')
+            const json = await resp.json()
+            setPosts(json)
+        })();
+    }, [])*/
+
+    const { Page, Bnext, Breturn, DeviceData, DeviceLastPage } = 
+    
+    useStaticPagination({
+        data:posts, 
+        sliceCell:1, 
+        sliceDesktop:3, 
+        sliceTv:4,
+        classStyle:"default",
+        arrowWeight:100
+    })
+    
     return(
         <div>
             <CustomHeading text={'Postagens da Scode'}/>
+            {Breturn} {Page}-{DeviceLastPage} {Bnext}
             <SimpleGrid columns={4} spacing={4}>
-            {posts.map(post => <PostCard 
+            {DeviceData.map(post => <PostCard 
                 key={post.id} 
                 post_id={post.id} 
                 postDescription={post.description} 
---
+++ app/src/hooks/useStaticPagination/useStaticPagination.tsx
@@ -0,0 +1,187 @@
+import { useEffect, useState } from 'react'
+import css from './useStaticPagination.module.css'
+
+interface IMainFactory {
+    DeviceName:string,
+    ReturnPage: Function
+    NextPage: Function
+    DeviceSlice: string,
+    DeviceData: Array<any>
+    DeviceLastPage:number
+}
+
+type Range = [100,200,300,400,500]
+
+type Styles = ['default','redCircle', 'orangeCircle']
+
+
+interface IProps<T> {
+    data:Array<T>
+    sliceCell:number
+    sliceDesktop:number
+    sliceTv:number
+    arrowWeight:Range[number]
+    classStyle?:Styles[number]
+}
+
+interface IDevice {
+    device:string
+    lastPage:number
+    slice:string
+    Data:Array<any>
+}
+
+const Arrows = {
+    right:{100:'🡢',200:'🡪', 300:'🡲', 400:'🡺',500:'🢂'},
+    left: {100:'🡠',200:'🡨', 300:'🡰', 400:'🡸', 500:'🢀'}
+}
+
+export default function useStaticPagination({data, sliceCell, sliceDesktop, sliceTv, arrowWeight, classStyle}:IProps<Object>) {
+    const [ width, setWidth] = useState(0)
+    const [ Start, setStart ] = useState(0)
+    const [ Page, setPage ] = useState(1)
+    const [ MaxItems ] = useState(data.length)
+    const [ SliceCell, setSliceCell ] = useState(sliceCell)
+    const [ SliceDesktop, setSliceDesktop ] = useState(sliceDesktop)
+    const [ SliceTv, setSliceTv ] = useState(sliceTv)
+
+   
+    useEffect(() => {
+        const w = window.innerWidth
+    
+        setWidth(w)
+    }, [])
+
+    const Device = ():IDevice => {
+   
+        if(width < 500){
+            const lastPage = Math.ceil(MaxItems / sliceCell)
+            const slice = `slice(${Start}-${SliceCell})`
+            const Data = data.slice(Start, SliceCell)
+
+            return {
+                device:'cell',
+                lastPage,
+                slice,
+                Data
+            }
+        }
+        if(width < 1300 && width > 500){
+            const lastPage = Math.ceil(MaxItems / sliceDesktop)
+            const slice = `slice(${Start}-${SliceDesktop})`
+            const Data = data.slice(Start, SliceDesktop)
+
+            return {
+                device:'desktop',
+                lastPage,
+                slice,
+                Data
+            }
+        }
+        else {
+            const lastPage = Math.ceil(MaxItems / sliceTv)
+            const slice = `slice(${Start}-${SliceTv})`
+            const Data = data.slice(Start, SliceTv)
+
+            return {
+                device:'tv',
+                lastPage,
+                slice,
+                Data
+            }
+        }
+    }
+
+    const MainFactory = ():IMainFactory => {
+        const { device, lastPage, slice, Data} = Device()
+
+        if(device === 'cell'){
+           
+            return {
+                DeviceName: device,
+                ReturnPage: () => { 
+                    setStart(Start - sliceCell)
+                    setSliceCell(SliceCell - sliceCell)
+                    setPage(Page - 1)
+                },
+                NextPage:() => {
+                    setStart(Start + sliceCell)
+                    setSliceCell(sliceCell + SliceCell)
+                    setPage(Page + 1)
+                },
+                DeviceSlice: slice,
+                DeviceData: Data,
+                DeviceLastPage: lastPage,
+            }
+        }
+        if(device === 'desktop'){
+           
+            return {
+                DeviceName:device,
+                ReturnPage: () => {
+                    setStart(Start - sliceDesktop)
+                    setSliceDesktop(SliceDesktop - sliceDesktop)
+                    setPage(Page - 1)
+                },
+                NextPage:() => {
+                    setStart(Start + sliceDesktop)
+                    setSliceDesktop(sliceDesktop + SliceDesktop)
+                    setPage(Page + 1)
+                },
+                DeviceSlice:slice,
+                DeviceData:Data,
+                DeviceLastPage:lastPage,
+            }
+        }
+        if(device === 'tv'){
+
+            return {
+                DeviceName:device,
+                ReturnPage:() => {
+                    setStart(Start - sliceTv)
+                    setSliceTv(SliceTv - sliceTv)
+                    setPage(Page - 1)
+                },
+                NextPage:() => {
+                    setStart(Start + sliceTv)
+                    setSliceTv(sliceTv + SliceTv)
+                    setPage(Page + 1)
+                },
+                DeviceSlice:slice,
+                DeviceData:Data,
+                DeviceLastPage:lastPage,
+            }
+        }
+    }
+    const { DeviceData, DeviceLastPage, DeviceName, DeviceSlice, NextPage, ReturnPage } = MainFactory()
+
+    const Bnext = 
+    
+    <button id={'Bnext'} name={'buttons'} onClick={() => NextPage()} className={Page === DeviceLastPage ? css['disabled']:css[classStyle ?? 'default']}>
+        <span>
+            {Arrows.right[arrowWeight]}
+        </span>
+    </button>
+
+    const Breturn =
+
+    <button id={'Breturn'} name={'b'} onClick={() => ReturnPage()} className={Page === 1 ? css['disabled']: css[classStyle ?? 'default']}>
+        <span>
+            {Arrows.left[arrowWeight]}
+        </span>
+    </button>
+
+    return {
+        Result:MainFactory(),
+        DeviceName,
+        DeviceSlice,
+        Start, 
+        Page,
+        NextPage,
+        ReturnPage,
+        DeviceData,
+        DeviceLastPage,
+        Bnext,
+        Breturn
+    }
+}
